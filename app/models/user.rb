9203485960c5559c51dd96219346ba2005321d6f
katsu9246 <katsu9246@gmail.com>
2022-07-11 19:50:46 +0900

    単体テストコードの実装（修正２回目）

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :items
+  # has_many :items
 
-  validates :first_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/ }# ここがユーザー本名全角かな、カナ、漢字の正規表現
-  validates :last_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/ }# ここがユーザー本名全角かな、カナ、漢字の正規表現
+  validates :first_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]+\z/ }# ここがユーザー本名全角かな、カナ、漢字の正規表現
+  validates :last_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]+\z/ }# ここがユーザー本名全角かな、カナ、漢字の正規表現
   validates :first_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/ }# ここがユーザー本名全角カタカナの正規表現
   validates :last_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/ }# ここがユーザー本名全角カタカナの正規表現
   validates :nick_name, presence: true
