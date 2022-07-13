a8789c5ec4f2f5f9eac73a1d2c9db85753a0c0bf
katsu9246 <katsu9246@gmail.com>
2022-07-13 21:45:05 +0900

    販売手数料計算のjavascriptの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,6 +5,10 @@ class Item < ApplicationRecord
   belongs_to :delivery_charge
   belongs_to :shipment_source
   has_one_attached :image
+  with_options presence: true, format: { with: /\A[0-9]+\z/ } do
+    validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 },
+                      presence: { message: "can't be blank"}
+  end
 
   validates :item_name, presence: true
   validates :category_id, presence: true
@@ -25,4 +29,5 @@ class Item < ApplicationRecord
   validates :delivery_charge_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :shipment_source_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :days_to_ship_id, numericality: { other_than: 1 , message: "can't be blank"}
+
 end
