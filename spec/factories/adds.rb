693954f9ab093b0b0ff99094433f1021ccaffcde
katsu9246 <katsu9246@gmail.com>
2022-07-21 11:36:37 +0900

    単体テストコードの実装

--- spec/factories/adds.rb
+++ spec/factories/adds.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :add do
-    
+    post_code { '123-4567' }
+    shipment_source_id { '2' }
+    municipalities { '横浜市緑区' }
+    address { '青山1-1-1' }
+    building_name { '柳ビル101' }
+    phone_number { '09012345678' }
   end
 end
--- spec/models/purchase_spec.rb
+++ spec/models/purchase_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Purchase, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+  user = FactoryBot.create(:user)
+  item = FactoryBot.create(:item)
+    @purchase = FactoryBot.create(:purchase, user_id: user.id, item_id: item.id)
+    sleep(1)
+  end
+  context '購入情報が保存できるとき' do
+    it '全ての項目が正しく入力されていれば保存できること' do
+      expect(@purchase).to be_valid
+    end
+  end
+  context '購入情報が保存できないとき' do
+    it 'ユーザーIDが紐付いていなければ保存できないこと' do
+      @purchase.user = nil
+      @purchase.valid?
+      expect(@purchase.errors.full_messages).to include('User must exist')
+    end
+    it 'アイテムIDが紐付いていなければ保存できないこと' do
+      @purchase.item = nil
+      @purchase.valid?
+      expect(@purchase.errors.full_messages).to include('Item must exist')
+    end
+  end
 end
